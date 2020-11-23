6cbe139129e8bc9d6c1b643fa2af94a2c3307818
Ivo Minic <ivo.minic@gmail.com>
2020-11-23 06:35:11 +0100

    Drugi javio grešku javax.net.ssl.SSLHandshakeException: No subject alternative names present Probati u etc/hosts fajlu da dodam naziv i pozovem po tom nazivu ovu ip adresu https://www.ibm.com/support/knowledgecenter/SS3NGB_5.1.0.10/ioc/ts_liberty_ip.html

--- grails-app/controllers/soapws/TestController.groovy
+++ grails-app/controllers/soapws/TestController.groovy
@@ -34,6 +34,8 @@ class TestController {
         Certificate publicSertifikat = certificateFactory.generateCertificate(fileInputStreamPublic)
         fileInputStreamPublic.close()
 
+        println publicSertifikat
+
 
         //try {
         FileInputStream fileInputStream = new FileInputStream(new File(request.getSession().getServletContext().getRealPath('/sertifikati/EpeticijePK.cer.pfx')))
@@ -50,7 +52,7 @@ class TestController {
         }
         X509Certificate certificate = (X509Certificate)keyStore.getCertificate(alijas)
         //println "certificate Subject " + certificate.getSubjectDN()
-        //println "certificate" + certificate
+        println "certificate" + certificate
         println "private key " + privateKey
 
         Signature privateSignature = Signature.getInstance("SHA1withRSA")
@@ -98,6 +100,9 @@ class TestController {
                 return true
             }
         }
+
+        // Install the all-trusting host verifier
+        HttpsURLConnection.setDefaultHostnameVerifier(allHostsValid);
 
         SOAPClient client = new SOAPClient("${servis1}?wsdl")
         //SOAPClient client = new SOAPClient()
@@ -224,6 +229,9 @@ class TestController {
             }
         }
 
+        // Install the all-trusting host verifier
+        HttpsURLConnection.setDefaultHostnameVerifier(allHostsValid);
+
         SOAPClient client = new SOAPClient("${servis1}?wsdl")
         //SOAPClient client = new SOAPClient()
         //client.httpClient.sslTrustStoreFile = publicSertifikat
@@ -324,7 +332,214 @@ class TestController {
             }
         }
 
+        // Install the all-trusting host verifier
+        HttpsURLConnection.setDefaultHostnameVerifier(allHostsValid);
+
         SOAPClient client = new SOAPClient("${servis1}?wsdl")
+        //SOAPClient client = new SOAPClient()
+        //client.httpClient.sslTrustStoreFile = publicSertifikat
+        //client.httpClient.sslTrustStoreFile = privateKey
+        //client.httpClient.sslTrustStorePassword ="123456789"
+        //client.httpClient.sslTrustAllCerts = true
+        SOAPResponse response = client.send(
+                """<?xml version='1.0' encoding='UTF-8'?>
+       <SOAP-ENV:Envelope xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/"
+    xmlns:do="https://10.61.4.10/servicev2/dokumenta-lk.php">
+    <SOAP-ENV:Header/>
+    <SOAP-ENV:Body>
+        <do:vratiDokumentLK SOAP-ENV:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">
+            <DokumentIN>
+                <user>epeticija</user>
+                <password>epet0812</password>
+                <pravni_osnov>24</pravni_osnov>
+                <osnov>24</osnov>
+                <mbr>1703982210261</mbr>
+                <broj_dokumenta>821049285</broj_dokumenta>
+                <organ_izdavanja></organ_izdavanja>
+                <datum_izdavanja>2010</datum_izdavanja>
+                <datum_zastite></datum_zastite>
+                <potpis>lrBsu4qFR2MVGjw9FAzSw7AGHzRQWX+cFmYhXxsiry9YyyflzIzkvCUJMJaga0aZsg3Qjqz5g0LPHkoy6BcV4gioFYVcMQiJBf+w0v6XligUAkqrbyquA2JKKTUaNLmp3OEpqbf9RQHY0zXCglczOjABv17GukMeBuKYMHrSOv24kToYfGDjlONk5CLYkWUvoHbodMV+alTS2elnF1iTFpR+8waZHhU/4gIGm/+BVdkY3iq+Cx2PsDVInYvonk/16GYOYbRdsa0uTtuDAmYpgFXZLYBVMlGFxKhA3ERDIcoqQ3tLLc4J42RVnE8HAmTe16lP7kUdxBmrvOrFoI7alg==</potpis>
+            </DokumentIN>
+        </do:vratiDokumentLK>
+    </SOAP-ENV:Body>
+</SOAP-ENV:Envelope>"""
+        )
+
+        render response
+
+    }
+
+    def treci() {
+        FileInputStream fileInputStreamPublic = new FileInputStream(new File(request.getSession().getServletContext().getRealPath('/sertifikati/Epeticije.cer')))
+        CertificateFactory certificateFactory = CertificateFactory.getInstance(X509)
+        Certificate publicSertifikat = certificateFactory.generateCertificate(fileInputStreamPublic)
+        fileInputStreamPublic.close()
+
+        FileInputStream fileInputStream = new FileInputStream(new File(request.getSession().getServletContext().getRealPath('/sertifikati/EpeticijePK.cer.pfx')))
+
+        KeyStore keyStore = KeyStore.getInstance(KEYSTORE_TYPE)
+        keyStore.load(fileInputStream, KEYSTORE_PASS.toCharArray())
+        PrivateKey privateKey
+        //println keyStore.aliases().getProperties()
+        String alijas = ""
+        keyStore.aliases().each {a->
+            alijas = a
+            privateKey = (PrivateKey) keyStore.getKey(a, KEYSTORE_PASS.toCharArray())
+        }
+        X509Certificate certificate = (X509Certificate)keyStore.getCertificate(alijas)
+
+        Signature privateSignature = Signature.getInstance("SHA1withRSA")
+
+        privateSignature.initSign(privateKey)
+
+        String user = "epeticija"
+        String password = "epet0812"
+        String pravni_osnov = "24"
+        String osnov = "24"
+        String mbr = "1703982210261"
+        String broj_dokumenta = "821049285"
+        String organ_izdavanja = "" //Ovdje kod za organ izdavanja, vraća ga servis
+        String datum_izdavanja = "2010" //Godina izdavanja dokumenta
+        String datum_zastite = "" //šalje se prazan string
+
+        String stringZaPotpis = user + " , " + password + " , " + pravni_osnov + " , " + osnov + " , " + mbr + " , " + broj_dokumenta + " , " + organ_izdavanja + " , " + datum_izdavanja + " , " + datum_zastite
+        privateSignature.update(stringZaPotpis.getBytes())
+
+        byte[] signature = privateSignature.sign()
+        String potpis = Base64.getEncoder().encodeToString(signature)
+
+        println "potpis    " + potpis
+
+        // Create a trust manager that does not validate certificate chains
+        TrustManager[] trustAllCerts = [ new X509TrustManager() {
+            public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                return null
+            }
+            public void checkClientTrusted(X509Certificate[] certs, String authType) {
+            }
+            public void checkServerTrusted(X509Certificate[] certs, String authType) {
+            }
+        }
+        ]
+
+// Install the all-trusting trust manager
+        SSLContext sc = SSLContext.getInstance("SSL")
+        sc.init(null, trustAllCerts, new java.security.SecureRandom())
+        HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory())
+
+// Create all-trusting host name verifier
+        HostnameVerifier allHostsValid = new HostnameVerifier() {
+            public boolean verify(String hostname, SSLSession session) {
+                return true
+            }
+        }
+
+        // Install the all-trusting host verifier
+        HttpsURLConnection.setDefaultHostnameVerifier(allHostsValid);
+
+        SOAPClient client = new SOAPClient("https://gov-epet/servicev2/dokumenta-lk.php")
+        //SOAPClient client = new SOAPClient()
+        //client.httpClient.sslTrustStoreFile = publicSertifikat
+        //client.httpClient.sslTrustStoreFile = privateKey
+        //client.httpClient.sslTrustStorePassword ="123456789"
+        //client.httpClient.sslTrustAllCerts = true
+        SOAPResponse response = client.send(
+                """<?xml version='1.0' encoding='UTF-8'?>
+       <SOAP-ENV:Envelope xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/"
+    xmlns:do="https://10.61.4.10/servicev2/dokumenta-lk.php">
+    <SOAP-ENV:Header/>
+    <SOAP-ENV:Body>
+        <do:vratiDokumentLK SOAP-ENV:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">
+            <DokumentIN>
+                <user>epeticija</user>
+                <password>epet0812</password>
+                <pravni_osnov>24</pravni_osnov>
+                <osnov>24</osnov>
+                <mbr>1703982210261</mbr>
+                <broj_dokumenta>821049285</broj_dokumenta>
+                <organ_izdavanja></organ_izdavanja>
+                <datum_izdavanja>2010</datum_izdavanja>
+                <datum_zastite></datum_zastite>
+                <potpis>lrBsu4qFR2MVGjw9FAzSw7AGHzRQWX+cFmYhXxsiry9YyyflzIzkvCUJMJaga0aZsg3Qjqz5g0LPHkoy6BcV4gioFYVcMQiJBf+w0v6XligUAkqrbyquA2JKKTUaNLmp3OEpqbf9RQHY0zXCglczOjABv17GukMeBuKYMHrSOv24kToYfGDjlONk5CLYkWUvoHbodMV+alTS2elnF1iTFpR+8waZHhU/4gIGm/+BVdkY3iq+Cx2PsDVInYvonk/16GYOYbRdsa0uTtuDAmYpgFXZLYBVMlGFxKhA3ERDIcoqQ3tLLc4J42RVnE8HAmTe16lP7kUdxBmrvOrFoI7alg==</potpis>
+            </DokumentIN>
+        </do:vratiDokumentLK>
+    </SOAP-ENV:Body>
+</SOAP-ENV:Envelope>"""
+        )
+
+        render response
+
+    }
+
+    def cetvrti() {
+        FileInputStream fileInputStreamPublic = new FileInputStream(new File(request.getSession().getServletContext().getRealPath('/sertifikati/Epeticije.cer')))
+        CertificateFactory certificateFactory = CertificateFactory.getInstance(X509)
+        Certificate publicSertifikat = certificateFactory.generateCertificate(fileInputStreamPublic)
+        fileInputStreamPublic.close()
+
+        FileInputStream fileInputStream = new FileInputStream(new File(request.getSession().getServletContext().getRealPath('/sertifikati/EpeticijePK.cer.pfx')))
+
+        KeyStore keyStore = KeyStore.getInstance(KEYSTORE_TYPE)
+        keyStore.load(fileInputStream, KEYSTORE_PASS.toCharArray())
+        PrivateKey privateKey
+        //println keyStore.aliases().getProperties()
+        String alijas = ""
+        keyStore.aliases().each {a->
+            alijas = a
+            privateKey = (PrivateKey) keyStore.getKey(a, KEYSTORE_PASS.toCharArray())
+        }
+        X509Certificate certificate = (X509Certificate)keyStore.getCertificate(alijas)
+
+        Signature privateSignature = Signature.getInstance("SHA1withRSA")
+
+        privateSignature.initSign(privateKey)
+
+        String user = "epeticija"
+        String password = "epet0812"
+        String pravni_osnov = "24"
+        String osnov = "24"
+        String mbr = "1703982210261"
+        String broj_dokumenta = "821049285"
+        String organ_izdavanja = "" //Ovdje kod za organ izdavanja, vraća ga servis
+        String datum_izdavanja = "2010" //Godina izdavanja dokumenta
+        String datum_zastite = "" //šalje se prazan string
+
+        String stringZaPotpis = user + " , " + password + " , " + pravni_osnov + " , " + osnov + " , " + mbr + " , " + broj_dokumenta + " , " + organ_izdavanja + " , " + datum_izdavanja + " , " + datum_zastite
+        privateSignature.update(stringZaPotpis.getBytes())
+
+        byte[] signature = privateSignature.sign()
+        String potpis = Base64.getEncoder().encodeToString(signature)
+
+        println "potpis    " + potpis
+
+        // Create a trust manager that does not validate certificate chains
+        TrustManager[] trustAllCerts = [ new X509TrustManager() {
+            public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                return null
+            }
+            public void checkClientTrusted(X509Certificate[] certs, String authType) {
+            }
+            public void checkServerTrusted(X509Certificate[] certs, String authType) {
+            }
+        }
+        ]
+
+// Install the all-trusting trust manager
+        SSLContext sc = SSLContext.getInstance("SSL")
+        sc.init(null, trustAllCerts, new java.security.SecureRandom())
+        HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory())
+
+// Create all-trusting host name verifier
+        HostnameVerifier allHostsValid = new HostnameVerifier() {
+            public boolean verify(String hostname, SSLSession session) {
+                return true
+            }
+        }
+
+        // Install the all-trusting host verifier
+        HttpsURLConnection.setDefaultHostnameVerifier(allHostsValid);
+
+        SOAPClient client = new SOAPClient("https://gov-epet/servicev2/dokumenta-lk.php")
         //SOAPClient client = new SOAPClient()
         //client.httpClient.sslTrustStoreFile = publicSertifikat
         //client.httpClient.sslTrustStoreFile = privateKey
